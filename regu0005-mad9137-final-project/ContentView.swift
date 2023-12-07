0066daebb3fdffc886c098c449417791bea35858
“regu0005@algonquinlive.com” <regu0005@algonquinlive.com>
2023-12-07 13:45:20 -0500

    Project init

---
+++ regu0005-mad9137-final-project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  regu0005-mad9137-final-project
+//
+//  Created by Gustavo Reguerin on 2023-12-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ regu0005-mad9137-final-project/regu0005_mad9137_final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  regu0005_mad9137_final_projectApp.swift
+//  regu0005-mad9137-final-project
+//
+//  Created by Gustavo Reguerin on 2023-12-07.
+//
+
+import SwiftUI
+
+@main
+struct regu0005_mad9137_final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
