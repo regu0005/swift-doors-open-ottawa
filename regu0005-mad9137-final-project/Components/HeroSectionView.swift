12f00c654651ea4f35c07a29b307a763a7670af4
“regu0005@algonquinlive.com” <regu0005@algonquinlive.com>
2023-12-08 17:52:59 -0500

    Homepage improved, hero section with zoom effect, and added Custom Tab Bar

--- regu0005-mad9137-final-project/Components/HeroSectionView.swift
+++ regu0005-mad9137-final-project/Components/HeroSectionView.swift
@@ -9,65 +9,152 @@ import SwiftUI
 
 struct HeroSectionView: View {
     @ObservedObject var buildingsDataModel : BuildingsDataModel
+        
+    @State private var currentImageIndex = 0
+    @State private var imageLoadState = ImageLoadState.loading
+    @State private var imageScale: CGFloat = 0.9
     
-    @State private var isMainImageLoaded = false
-    @State private var imageOpacity = 0.0
-    
-    @State private var selectedBuildingIndex = 0
+    let timer = Timer.publish(every: 4, on: .main, in: .common).autoconnect()
     
     var body: some View {
-        
-        TabView(selection: $selectedBuildingIndex) {
-                    ForEach(buildingsDataModel.getTopThreeVisitedBuildings(), id: \.id) { building in
-                        ZStack(alignment: .topTrailing) {
-                            AsyncImage(url: URL(string: building.image ?? "")) { phase in
-                                switch phase {
-                                case .empty:
-                                    Color.clear
-                                        .frame(maxWidth: .infinity, maxHeight: 260)
-                                    ProgressView("...")
-                                        .font(.headline)
-                                        .bold()
-                                    
-                                case .success(let image):
-                                    image.resizable()
-                                        .aspectRatio(contentMode: .fill)
-                                        .transition(.opacity)
-                                        .animation(.easeInOut(duration: 0.5), value: selectedBuildingIndex)
-                                case .failure:
-                                    Image("placeholder_image") // Replace with your placeholder image
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fill)
-                                @unknown default:
-                                    EmptyView()
+        ZStack {
+            if let building = buildingsDataModel.getTopThreeVisitedBuildings().enumerated().first(where: { $0.offset == currentImageIndex })?.element {
+
+                AsyncImage(url: URL(string: building.image ?? "")) { phase in
+                    switch phase {
+                    case .empty:
+                        Color.clear
+                            .frame(maxWidth: .infinity, maxHeight: 260)
+                        ProgressView()
+                            .font(.headline)
+                            .bold()
+                            .onAppear {
+                                imageLoadState = .loading
+                                imageScale = 1.0
+                                print("loading empty...");
+                            }
+
+                    case .success(let image):
+                        image.resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .scaleEffect(imageScale)
+                            .transition(.opacity)
+                            .onAppear {
+                                if imageLoadState != .loaded {
+                                    withAnimation(.easeIn(duration: 4.2)) {
+                                        imageScale = 1.1
+                                        imageLoadState = .loaded
+                                        print("loading success...")
+                                    }
                                 }
                             }
-                            .frame(maxWidth: .infinity, maxHeight: 260)
-                            .clipped()
 
-                            VStack(alignment: .trailing, spacing: 10) {
-                                Text(building.name)
-                                    .font(Font.system(size: 12))
-//                                    .fontWeight(.bold)
-                                    .italic()
-                                    .padding(.top, 264)
-                                    .padding(.horizontal, 16)
-
-//                                Text(building.address)
-//                                    .font(Font.system(size: 12))
-//                                    .foregroundColor(.secondary)
-//                                    .padding(.top, -10)
-//                                    .padding(.horizontal, 20)
+                    case .failure:
+                        Image("placeholder_image")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .onAppear {
+                                imageLoadState = .failed
+                                imageScale = 1.0
                             }
-                        }
-                        .tag(building.id)
+
+                    @unknown default:
+                        EmptyView()
                     }
                 }
-                .tabViewStyle(PageTabViewStyle())
-                .frame(height: 278)
-                .padding(.top,-5)
-                .edgesIgnoringSafeArea(.all)
+                .id(currentImageIndex)
+                .frame(maxWidth: .infinity, maxHeight: 260)
+                .clipped()
+                .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+                .onReceive(timer) { _ in
+                    withAnimation(.easeInOut(duration: 3.0)) {
+                        currentImageIndex = (currentImageIndex + 1) % buildingsDataModel.getTopThreeVisitedBuildings().count
+                        imageLoadState = .loading
+                        imageScale = 1.0
+                    }
+                } // end asyncimage
+//                VStack(alignment: .trailing, spacing: 10) {
+//                    Text(building.name)
+//                        .font(Font.system(size: 14))
+//                        .italic()
+//                        .padding(.top, 280)
+//                        .opacity(textOpacity)
+//                        .onAppear {
+//                            withAnimation(.smooth(duration: 0.5)) {
+//                                textOpacity = 1.0
+//                            }
+//                        }
+//                            .padding(.leading, 40)
+//                            .padding(.horizontal, 16)
+                
+                ////                                Text(building.address)
+                ////                                    .font(Font.system(size: 12))
+                ////                                    .foregroundColor(.secondary)
+                ////                                    .padding(.top, -10)
+                ////                                    .padding(.horizontal, 20)
+                //}
+            }
+        }
+        .frame(height: 278)
+        .padding(.top, -10)
+        .edgesIgnoringSafeArea(.all)
+        
+//        TabView(selection: $selectedBuildingIndex)
+//        {
+//                ForEach(buildingsDataModel.getTopThreeVisitedBuildings(), id: \.id) { building in
+//                    ZStack(alignment: .topTrailing) {
+//                        AsyncImage(url: URL(string: building.image ?? "")) { phase in
+//                            switch phase {
+//                            case .empty:
+//                                Color.clear
+//                                    .frame(maxWidth: .infinity, maxHeight: 260)
+//                                ProgressView("...")
+//                                    .font(.headline)
+//                                    .bold()
+//                                
+//                            case .success(let image):
+//                                image.resizable()
+//                                    .aspectRatio(contentMode: .fill)
+//                                    .transition(.opacity)
+//                                    .animation(.easeInOut(duration: 0.5), value: selectedBuildingIndex)
+//                            case .failure:
+//                                Image("algonquin")
+//                                    .resizable()
+//                                    .aspectRatio(contentMode: .fill)
+//                            @unknown default:
+//                                EmptyView()
+//                            }
+//                        }
+//                        .frame(maxWidth: .infinity, maxHeight: 260)
+//                        .clipped()
+//
+//                        VStack(alignment: .trailing, spacing: 10) {
+//                            Text(building.name)
+//                                .font(Font.system(size: 12))
+////                                    .fontWeight(.bold)
+//                                .italic()
+//                                .padding(.top, 264)
+//                                .padding(.horizontal, 16)
+//
+////                                Text(building.address)
+////                                    .font(Font.system(size: 12))
+////                                    .foregroundColor(.secondary)
+////                                    .padding(.top, -10)
+////                                    .padding(.horizontal, 20)
+//                        }
+//                    }
+//                    .tag(building.id)
+//                } // end foreach
+//        } // end tabview
+//        .tabViewStyle(PageTabViewStyle())
+//        .frame(height: 278)
+//        .padding(.top,-5)
+//        .edgesIgnoringSafeArea(.all)
     }
+    
+    enum ImageLoadState {
+            case loading, loaded, failed
+        }
 }
 
 #Preview {
--- regu0005-mad9137-final-project/ContentView.swift
+++ regu0005-mad9137-final-project/ContentView.swift
@@ -18,31 +18,123 @@ struct ContentView: View {
     
     @EnvironmentObject var networkMonitor: NetworkMonitor
     
+    @State private var selectedTab: Tab = .home
+    
     var body: some View {
-        NavigationView {
-            if(buildingsDataModel.isLoading && 
-               categoriesDataModel.isLoading &&
-               languagesDataModel.isLoading
-            ) {
-                ProgressView("Loading...")
-                    .font(.headline)
-                    .bold()
-            }
-            else {
-                VStack {
-                    ScrollView {
-                        HeroSectionView(buildingsDataModel:buildingsDataModel)
-                        CategoriesScrollView(categoriesDataModel: categoriesDataModel)
-                        RandomBuildingsView(buildingsDataModel: buildingsDataModel)
-                        BuildingsByAmenitiesView()
-                    }
+        ZStack(alignment: .bottom) {
+            NavigationView {
+                if(buildingsDataModel.isLoading &&
+                   categoriesDataModel.isLoading &&
+                   languagesDataModel.isLoading
+                ) {
+                    ProgressView("Loading...")
+                        .font(.headline)
+                        .bold()
                 }
-                .edgesIgnoringSafeArea(.all)
-                .navigationBarTitle("Buildings Home", displayMode: .inline)
-                .navigationBarHidden(true)
+                else {
+                    VStack {
+                        ScrollView {
+                            HeroSectionView(buildingsDataModel:buildingsDataModel)
+                            CategoriesScrollView(categoriesDataModel: categoriesDataModel)
+                            RandomBuildingsView(buildingsDataModel: buildingsDataModel)
+                            BuildingsByAmenitiesView()
+                        }
+                    }
+                    .edgesIgnoringSafeArea(.all)
+                    .navigationBarTitle("Buildings Home", displayMode: .inline)
+                    .navigationBarHidden(true)
+                }
             }
+            
+            // Main content view
+               switch selectedTab {
+               case .home:
+                   HomeView()
+               case .search:
+                   SearchView()
+               case .map:
+                   MapView()
+               case .favorites:
+                   FavoritesView()
+               case .about:
+                   AboutView()
+               }
+
+            HStack{
+                CustomTabBar(selectedTab: $selectedTab)
+                    .background(Color.white.opacity(0.85))
+                    .cornerRadius(15)
+                    .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+                    .padding(.horizontal, 30)
+                    .padding(.bottom, 1)
+                    .padding(.top, 15)
+            }
+            .background(Color.white.opacity(0.92))
         }
         
+        
+    } // end body view
+    
+    enum Tab {
+            case home, search, map, favorites, about
+        }
+}
+
+struct CustomTabBar: View {
+    @Binding var selectedTab: ContentView.Tab
+
+    var body: some View {
+        HStack {
+            TabBarButton(iconName: "house", tab: .home)
+            TabBarButton(iconName: "magnifyingglass", tab: .search)
+            TabBarButton(iconName: "map", tab: .map)
+            TabBarButton(iconName: "heart", tab: .favorites)
+            TabBarButton(iconName: "info.bubble", tab: .about)
+        }
+        .padding(.horizontal,20)
+        .padding(.bottom,15)
+        .padding(.top,15)
+        .cornerRadius(15)
+        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+    }
+
+    func TabBarButton(iconName: String, tab: ContentView.Tab) -> some View {
+        Button(action: {
+            withAnimation {
+                selectedTab = tab
+            }
+        }) {
+            Image(systemName: iconName)
+                .font(.system(size: 22))
+                .foregroundColor(selectedTab == tab ? .blue : .gray)
+                .padding()
+        }
+    }
+}
+
+struct HomeView: View {
+    var body: some View {
+        Text("Home")
+    }
+}
+struct SearchView: View {
+    var body: some View {
+        Text("Search")
+    }
+}
+struct MapView: View {
+    var body: some View {
+        Text("Map")
+    }
+}
+struct FavoritesView: View {
+    var body: some View {
+        Text("Favorites")
+    }
+}
+struct AboutView: View {
+    var body: some View {
+        Text("About")
     }
 }
 
