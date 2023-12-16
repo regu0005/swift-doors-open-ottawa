f8e21153ca900b9ab169c52cc2506dbe805eb0f6
“regu0005@algonquinlive.com” <regu0005@algonquinlive.com>
2023-12-16 16:25:55 -0500

    Uploading NetworkMonitor file missed in the repository

---
+++ regu0005-mad9137-final-project/Services/NetworkMonitor.swift
@@ -0,0 +1,42 @@
+//
+//  NetworkMonitor.swift
+//  regu0005-mad9137-final-project
+//
+//  Created by Gustavo Reguerin on 2023-12-07.
+//
+
+import Foundation
+
+import Network
+
+class NetworkMonitor: ObservableObject {
+    
+  private let networkMonitor = NWPathMonitor()
+    
+  private let workerQueue = DispatchQueue(label: "Monitor")
+    
+  var isConnected = false
+    
+  init() {
+      
+    networkMonitor.pathUpdateHandler = { path in
+        
+      self.isConnected = path.status == .satisfied
+        
+      Task {
+          
+        await MainActor.run {
+            
+          self.objectWillChange.send()
+            
+        }
+          
+      }
+        
+    }
+      
+    networkMonitor.start(queue: workerQueue)
+      
+  }
+    
+}
