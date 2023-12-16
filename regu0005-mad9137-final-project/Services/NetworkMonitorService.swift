fb4a6af1867c3be927e33c0ebffc375564212550
“regu0005@algonquinlive.com” <regu0005@algonquinlive.com>
2023-12-16 16:37:24 -0500

    NetworkMonitor changed to NetworkMonitorService fixing the issue of the file missed

---
+++ regu0005-mad9137-final-project/Services/NetworkMonitorService.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkMonitorService.swift
+//  regu0005-mad9137-final-project
+//
+//  Created by Gustavo Reguerin on 2023-12-16.
+//
+
+import SwiftUI
+
+struct NetworkMonitorService: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NetworkMonitorService()
+}
